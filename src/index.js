8a21e401ff04be5b2366ef77b9829e9b6fa39ee4
Siddhesh More <30049851+bearded-impala@users.noreply.github.com>
2021-05-09 19:38:25 +0530

    1.0.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import CowinNotification from './CowinNotification';
-import ReactGA from 'react-ga';
 
-ReactGA.initialize('G-37ZWS4YK2P');
 render(<CowinNotification />, document.getElementById('root'));
